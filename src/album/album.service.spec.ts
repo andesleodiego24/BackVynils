d1933563ee77ea800660b20a25fe0845d7638604
andesleodiego24 <dl.bustamante@uniandes.edu.co>
2024-11-03 22:41:32 -0500

    fix: se remueve importacion sin uso

--- src/album/album.service.spec.ts
+++ src/album/album.service.spec.ts
@@ -1,7 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { AlbumDTO } from './album.dto';
 import { Album } from './album.entity';
 import { AlbumService } from './album.service';
 
